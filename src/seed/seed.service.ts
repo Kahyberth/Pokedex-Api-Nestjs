32cec1c6d1cb7d6ac4a52209c84ef59d5b470c25
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2024-05-11 18:30:20 -0500

    chore: Refactor SeedService to handle larger number of Pokemon

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -5,19 +5,22 @@ import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
-  
+
   private readonly axios: AxiosInstance = axios;
 
-  constructor(private pokemonService: PokemonService) {}
+  constructor(private pokemonService: PokemonService) { }
 
   async executeSeed() {
-    const { data } = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
 
-   data.results.forEach(async({name, url}) => {
-    const fragments = url.split("/");
-    const no = +fragments[fragments.length - 2];
-    await this.pokemonService.create({name, no})
-   })
+    const { data } = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+    const insertPromisesArray = []
+    data.results.forEach(async ({ name, url }) => {
+      const fragments = url.split("/");
+      const no = +fragments[fragments.length - 2];
+      insertPromisesArray.push(this.pokemonService.create({ name, no }));
+    })
+
+    await Promise.all(insertPromisesArray);
 
     return 'Seed Executed';
   }
