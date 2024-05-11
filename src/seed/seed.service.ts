1dde5ef04b03afdd544df2742b95af66804097e5
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2024-05-11 18:57:41 -0500

    chore: provider http-adapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios'
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { PokemonService } from 'src/pokemon/pokemon.service';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
 
-  constructor(private pokemonService: PokemonService) { }
+
+  constructor(private pokemonService: PokemonService, private readonly http: AxiosAdapter) { }
 
   async executeSeed() {
 
-    const { data } = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=750')
     const insertPromisesArray = []
+
     data.results.forEach(async ({ name, url }) => {
       const fragments = url.split("/");
       const no = +fragments[fragments.length - 2];
